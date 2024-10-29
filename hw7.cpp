edd4bff764fad460a527779228bdff16b2f46e80
nhimsel <>
2024-10-29 12:12:52 -0500

    clean up code and add some comments

--- hw7.cpp
+++ hw7.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 using namespace std;
 
-
 struct Fraction
 {
     int num;
     int denom;
 };
+
 struct Fraction addFractions(struct Fraction f1, struct Fraction f2);
 int LCM (int a, int b);
 int GCD(int a, int b);
 
 int main()
 {
-    struct Fraction f1; f1.num=7; f1.denom = 10;
-    struct Fraction f2; f2.num = 8; f2.denom=12;
+    struct Fraction f1; f1.num=1; f1.denom = 2;
+    struct Fraction f2; f2.num = 3; f2.denom=4;
     struct Fraction ans = addFractions(f1,f2);
     cout << ans.num << "/"<< ans.denom << endl;
     return 0;
@@ -23,9 +23,13 @@ int main()
 struct Fraction addFractions(struct Fraction f1, struct Fraction f2)
 {
     struct Fraction ans;
-    int lcm = LCM(f1.denom, f2.denom);
+
+    //basic fraction addition without reduction
     ans.num = f1.num*f2.denom + f2.num*f1.denom;
     ans.denom = f1.denom*f2.denom;
+
+    //calculate the LCM and reduce the fraction
+    int lcm = LCM(f1.denom, f2.denom);
      if (lcm>1)
     {
         ans.num=(ans.num*lcm)/ans.denom;
@@ -41,6 +45,7 @@ int LCM(int a, int b)
 
 int GCD(int a, int b)
 {
+    //using euclidian algorithm
     if (a==0) return b;
     else if (b==0) return a;
     else return GCD (b,a%b);
